5aec65861e810647f81e7baba85575ef2010bbe6
petrlr14 <pedrodeveloper14@gmail.com>
2020-06-14 04:43:44 -0600

    added model aproximation on response

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
-from flask import Flask
+from flask import (Flask, jsonify, render_template)
+import joblib
 
 app = Flask(__name__)
+app.model = joblib.load('static/model.sav')
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('index.html')
+
+
+@app.route('/data')
+def data():
+    d = str(app.model.predict([[1, 1, 1]])).replace("[", "").replace("]","")
+    return jsonify({"data": d})
 
 
 if __name__ == '__main__':
